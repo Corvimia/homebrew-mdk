bb32372c77bc4efd1f8ba22c50d928fd144ace2a
Mia <lemoine.yoru@gmail.com>
2025-02-25 23:11:57 +0000

    update tap

--- mdk.rb
+++ mdk.rb
@@ -1,23 +1,13 @@
 # Homebrew Formula for mdk (Mia's Dev Kit)
-#
-# How to use:
-# 1. After creating a GitHub release, get the SHA256 with:
-#    curl -L https://github.com/Corvimia/mdk/archive/refs/tags/v0.0.1.tar.gz | shasum -a 256
-# 2. Update the sha256 value below with the actual checksum
-#
+
 class Mdk < Formula
   desc "Mia's Dev Kit"
   homepage "https://github.com/Corvimia/mdk"
-  url "https://github.com/Corvimia/mdk/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "9234c552e783544ae4e8fba30324299cab07c2c6d3812fe1fa8fc4c1fb3a472b"
+  url "https://github.com/Corvimia/mdk/releases/download/v0.0.1/mdk-v0.0.1.tar.gz"
+  sha256 "d3b33ade0b9359543fa4d4d4cc5bf39eef382e78a72ad780a59df905210df1c2"
   license "ISC"
 
-  depends_on "node"
-  depends_on "pnpm"
-
   def install
-    system "pnpm", "install"
-    system "make", "package"
     bin.install "mdk"
   end
 
